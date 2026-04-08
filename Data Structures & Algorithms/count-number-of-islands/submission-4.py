70ed109fccc9eb4122e81bbc973bf66d138a7694
bowerss5 <147757253+bowerss5@users.noreply.github.com>
2026-04-08 11:17:43 -0400

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.py
@@ -0,0 +1,42 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        seen = set() 
+
+
+        def neighbors(row, col):
+            ret = []
+
+            if row + 1 < len(grid):
+                ret.append((row + 1, col))
+            if col + 1 < len(grid[0]):
+                ret.append((row, col + 1))
+            if row - 1 >= 0:
+                ret.append((row - 1, col))
+            if col - 1 >= 0:
+                ret.append((row, col - 1))
+
+            return ret
+
+        def bfs(row, col):
+            queue = deque()
+            queue.append((row, col))
+            while len(queue) > 0:
+                v = queue.pop()
+                if not v in seen:
+                    # seen.add(v)
+                    grid[v[0]][v[1]] = "0"
+                    for w in neighbors(*v):
+                        if grid[w[0]][w[1]] == "0":
+                            continue
+                        queue.append(w)
+
+        count = 0
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == "0" or (row,col) in seen:
+                    continue
+                count += 1
+                bfs(row, col)
+                
+        return count
